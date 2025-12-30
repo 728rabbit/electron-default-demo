e08cb8f54da0095cf4842b294610e316de2cda4f
Kaiyun <kaiyunchan2012@hotmail.com>
2025-12-30 16:18:16 +0800

    Delete assets directory

--- assets/js/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Create a secure bridge that exposes only the necessary APIs to the webpage.
-contextBridge.exposeInMainWorld('electronAPI', {
-    openChildWindow: (viewName) => {
-        // window.electronAPI.openChildWindow('child');
-        return ipcRenderer.send('open-child-window', viewName);
-    }
-});
