4ceabebd0d4a7d15cc1f027112e2fa706559217b
Kaiyun <kaiyunchan2012@hotmail.com>
2025-12-30 19:18:11 +0800

    Add files via upload

--- preload.js
+++ preload.js
@@ -3,7 +3,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 // Create a secure bridge that exposes only the necessary APIs to the webpage.
 contextBridge.exposeInMainWorld('electronAPI', {
     openChildWindow: (viewName) => {
-        // window.electronAPI.openChildWindow('child');
+        // window.electronAPI.openChildWindow(...);
         return ipcRenderer.send('open-child-window', viewName);
+    },
+
+    showNotification: (name, content) => {
+        // window.electronAPI.showNotification(...);
+        return ipcRenderer.send('show-notification-message', {title: name, body: content});
     }
 });
